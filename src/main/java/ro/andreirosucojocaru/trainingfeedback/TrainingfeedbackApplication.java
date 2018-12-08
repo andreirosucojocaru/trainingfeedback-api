58b58de3496097509315d695001b1c1c3ffb36f9
Andrei Rosu-Cojocaru <andrei.rosucojocaru@gmail.com>
2018-12-08 12:32:57 +0200

    Initial commit

---
+++ src/main/java/ro/andreirosucojocaru/trainingfeedback/TrainingfeedbackApplication.java
@@ -0,0 +1,12 @@
+package ro.andreirosucojocaru.trainingfeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingfeedbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingfeedbackApplication.class, args);
+    }
+}
---
+++ src/test/java/ro/andreirosucojocaru/trainingfeedback/TrainingfeedbackApplicationTests.java
@@ -0,0 +1,16 @@
+package ro.andreirosucojocaru.trainingfeedback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrainingfeedbackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
